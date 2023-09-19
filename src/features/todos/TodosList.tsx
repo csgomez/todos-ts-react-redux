a11cd5fa0489e809c7f21a9fd17ad8627b3b27d2
Cesar <csgomez@users.noreply.github.com>
2023-09-19 18:26:59 -0500

    'No Todos' message is now a bit more detailed to reflect any filter modifiers

--- src/features/todos/TodosList.tsx
+++ src/features/todos/TodosList.tsx
@@ -1,11 +1,19 @@
 import { useSelector } from 'react-redux';
 import { selectFilteredTodos } from './todosSlice';
 import TodoItem from './TodoItem';
+import { selectStatusFilter } from '../filter/filterSlice';
 
 const TodosList = () => {
   const allTodos = useSelector(selectFilteredTodos);
+  const currentStatusFilter = useSelector(selectStatusFilter);
 
-  if (allTodos.length === 0) return <p>No Todos....</p>;
+  if (allTodos.length === 0) {
+    // "No completed Todos" || "No incomplete Todos" || "No Todos"
+    const messageModifier =
+      currentStatusFilter === 'all' ? '' : currentStatusFilter;
+
+    return <p>No {messageModifier} Todos....</p>;
+  }
 
   return (
     <div className="list-group gap-3">
