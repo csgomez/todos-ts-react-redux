1e9e1725f4a91d087a08911294e3e70568164179
Cesar Gomez <csgomez@users.noreply.github.com>
2023-09-27 18:31:35 -0500

    Edit and Delete buttons are now shown by default (instead of needing a cursor hover to be shown) for improved accessibility. Also added bottom margin to the list of todos.

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <main className="App">
       <h2 className="text-center fw-bold text-primary mt-4 mb-3">Todos</h2>
       <AddTodoForm />
-      <div className="bg-light py-4 shadow rounded-3 px-4 mt-3">
+      <div className="bg-light py-4 shadow rounded-3 px-4 mt-3 mb-4">
         <FilterInput />
         <TodosList />
       </div>
